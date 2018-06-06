26ec4df4b15f80539005f27c5b817873e66fcfbf
Morkalork <maffelu@gmail.com>
2018-06-06 15:36:17 +0200

    Added a birthday message if its his birthday!

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -5,19 +5,30 @@
         </header>
         <section>
             <p>En man bland pojkar, en legend bland stjärnor. En skinka på samhällets julbord.</p>
-            <h3><strong>Långskinkan!</strong></h3>
+            <h3><strong>Långskinkan! {{birthday}}</strong></h3>
         </section>
     </article>
 </template>
 
+<script>
+export default {
+  data () {
+    const today = new Date()
+    return {
+      birthday: (today.getMonth() === 5 && today.getDate() === 6) ? 'Grattis på födeseldagen!' : ''
+    }
+  }
+}
+</script>
+
 <style lang='scss' scoped>
-    h1 {
-      font-size: 3rem;
-    }
-    h3 {
-      font-size: 2rem;
-    }
-    p {
-      font-size: 1.2rem;
-    }
+h1 {
+  font-size: 3rem;
+}
+h3 {
+  font-size: 2rem;
+}
+p {
+  font-size: 1.2rem;
+}
 </style>
