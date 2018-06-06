df1e16300360061550267811943b15271d49e79f
Morkalork <maffelu@gmail.com>
2018-06-06 14:05:32 +0200

    Added testimonials

--- src/components/Testimonials.vue
+++ src/components/Testimonials.vue
@@ -3,15 +3,15 @@
         <header>
             <h1>Testimonials</h1>
         </header>
-        <section v-for='testimonial in testimonials' class='testimonial' :key='testimonial.id'>
+        <section v-for='(testimonial, index) in testimonials' class='testimonial' :key='index'>
             <div class='thumb'>
                 <figure>
                     <img :src='testimonial.img' alt='A ninja' />
                 </figure>
             </div>
             <div class='text'>
-                <p class='line'>Vilken kille!</p>
-                <p class='signum'> - Magnus Ferm</p>
+                <p class='line'>{{testimonial.line}}</p>
+                <p class='signum'> - {{testimonial.signum}}</p>
             </div>
         </section>
     </article>
@@ -19,16 +19,27 @@
 
 <script>
 import Ninja1 from '@/assets/ninja1.png'
+import Ninja2 from '@/assets/ninja2.png'
+import Ninja3 from '@/assets/ninja3.png'
 
 export default {
   data () {
     return {
       testimonials: [
         {
-          id: 1,
           img: Ninja1,
-          line: 'Vilken kille!',
+          line: 'Vilken kille, vilken skinka!',
           signum: 'Magnus Ferm'
+        },
+        {
+          img: Ninja2,
+          line: 'I vanliga fall undviker jag skinka men för långskinkan gör man ett undantag',
+          signum: 'The Agron'
+        },
+        {
+          img: Ninja3,
+          line: 'Varje gång Långskinkan kramar mig glömmer jag varför jag gick in i rummet',
+          signum: 'Martin Lindström'
         }
       ]
     }
@@ -41,10 +52,16 @@ export default {
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
+  max-width: 1024px;
+  margin: 0 auto;
+  @media screen and (max-width: 1024px){
+    max-width: 80%;
+  }
   .testimonial {
     display: flex;
     flex-direction: row;
+    text-align: left;
     img {
       box-shadow: 0 0 1rem #000;
     }
@@ -57,7 +74,7 @@ export default {
         &.line {
           font-weight: bold;
           text-transform: uppercase;
-          font-size: 1.5rem;
+          font-size: 1.2rem;
         }
         &.signum {
           font-size: 0.8rem;
