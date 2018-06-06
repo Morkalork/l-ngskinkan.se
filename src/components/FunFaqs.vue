e1c06b30a9c90254dfe7420997c7d06261a6c14b
Morkalork <maffelu@gmail.com>
2018-06-06 18:34:05 +0200

    Corrected a lie

--- src/components/FunFaqs.vue
+++ src/components/FunFaqs.vue
@@ -6,7 +6,7 @@
         <section>
             <p>Långskinkan står när de andra faller.</p>
             <p>Somliga går med trasiga skor. Inte långskinkan dock, han går i Converse.</p>
-            <p>Långskinkan är född på nationaldagen för han är en nationalklenod.</p>
+            <p>Långskinkan är <em>nästan</em> född på nationaldagen för han är en nationalklenod.</p>
         </section>
     </article>
 </template>
